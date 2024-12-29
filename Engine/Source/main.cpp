d1aa215d17cb8dc5e84f98e6fc3aeddf032ca329
Khairol Anwar <45542251+KhxiSaki@users.noreply.github.com>
2024-12-29 12:37:10 +0800

    Rename the window as RealityEngine

--- Engine/Source/main.cpp
+++ Engine/Source/main.cpp
@@ -8,32 +8,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	// Register the window class.
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	const wchar_t CLASS_NAME[] = L"RealityEngineClass";
 
 	WNDCLASS wc = { };
-
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
 
 	RegisterClass(&wc);
 
-	// Create the window.
-
 	HWND hwnd = CreateWindowEx(
-		0,                              // Optional window styles.
-		CLASS_NAME,                     // Window class
-		L"Learn to Program Windows",    // Window text
-		WS_OVERLAPPEDWINDOW,            // Window style
-
-		// Size and position
+		0,
+		CLASS_NAME,
+		L"RealityEngine", // Window title changed to "RealityEngine"
+		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-		NULL,       // Parent window    
-		NULL,       // Menu
-		hInstance,  // Instance handle
-		NULL        // Additional application data
+		NULL,
+		NULL,
+		hInstance,
+		NULL
 	);
 
 	if (hwnd == NULL)
@@ -42,8 +35,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	}
 
 	ShowWindow(hwnd, nCmdShow);
-
-	// Run the message loop.
 
 	MSG msg = { };
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
@@ -67,15 +58,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hwnd, &ps);
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-
 		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
 		EndPaint(hwnd, &ps);
 	}
 	return 0;
-
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
