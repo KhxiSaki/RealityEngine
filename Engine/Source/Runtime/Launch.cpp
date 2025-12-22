cc365fb3853668a6944bf5265963d7b53aec40e9
Khairol Anwar <45542251+KhxiSaki@users.noreply.github.com>
2025-12-22 19:35:22 +0800

    Following Vulkan tutorial-Window Creation

--- Engine/Source/Runtime/Launch.cpp
+++ Engine/Source/Runtime/Launch.cpp
@@ -1,8 +1,65 @@
-#include "Applications.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main()
-{
-	Applications app;
-	app.Run();
-	return 0;
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initwindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+
+    }
+
+    void initwindow()
+    {
+        //Initialize GLFW
+        glfwInit();
+
+        // Tell GLFW to not intiialize with OpenGL way
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        //disable window resizable
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        //Create window
+        window = glfwCreateWindow(WindowWidth, WindowHeight, "Vulkan", nullptr, nullptr);
+    }
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+private:
+    GLFWwindow* window;
+    const uint32_t WindowWidth = 800;
+    const uint32_t WindowHeight = 600;
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
